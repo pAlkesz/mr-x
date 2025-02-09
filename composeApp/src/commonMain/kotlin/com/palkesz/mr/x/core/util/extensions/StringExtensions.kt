8f5fa7943925b485e44b96df34e66539f3f9f574
pAlkesz <papppali2000@gmail.com>
2025-02-09 10:50:48 +0100

    fixing username length bug

--- composeApp/src/commonMain/kotlin/com/palkesz/mr/x/core/util/extensions/StringExtensions.kt
+++ composeApp/src/commonMain/kotlin/com/palkesz/mr/x/core/util/extensions/StringExtensions.kt
@@ -6,7 +6,7 @@ import doist.x.normalize.normalize
 
 fun String.validateAsName() = all { it.isLetter() || it == ' ' }
 
-fun String.validateAsUsername() = trim().all { it != ' ' } && length > 5 && length < 20
+fun String.validateAsUsername() = trim().all { it != ' ' } && length >= 5 && length <= 20
 
 fun String.validateAsEmail() =
     matches(Regex("[a-zA-Z0-9+._%\\-]{1,256}" + "@" + "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" + "(" + "\\." + "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" + ")+"))
